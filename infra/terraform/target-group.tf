b0e5e774c4c794645a5f71ea108f6369e15c1b86
Leonardo Delmiro <leodelmiroms@gmail.com>
2024-11-23 09:49:09 -0300

    fix: adicionando depends on para ec2 id

--- infra/terraform/target-group.tf
+++ infra/terraform/target-group.tf
@@ -1,20 +1,20 @@
 resource "aws_lb_target_group" "tg" {
-  name = "TG-${var.projectName}"
-  port = 30007
+  name        = "TG-${var.projectName}"
+  port        = 30007
   target_type = "instance"
-  protocol = "HTTP"
+  protocol    = "HTTP"
 
   vpc_id = data.aws_vpc.vpc.id
 
   health_check {
-    path = "/"
-    port = 30007
+    path    = "/"
+    port    = 30007
     matcher = "200"
   }
 }
 
 resource "aws_lb_target_group_attachment" "attach" {
   target_group_arn = aws_lb_target_group.tg.arn
-  target_id = data.aws_instance.ec2.id
-  port = 30007
+  target_id        = data.aws_instance.ec2.id
+  port             = 30007
 }
