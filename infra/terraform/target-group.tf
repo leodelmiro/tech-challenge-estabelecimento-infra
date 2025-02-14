bb8ac84db09808afad634d63456a5ee8718d0fc9
Leonardo Delmiro <leodelmiroms@gmail.com>
2025-02-13 21:15:34 -0300

    feat: adicionando attach de todos os microsserviços

--- infra/terraform/target-group.tf
+++ infra/terraform/target-group.tf
@@ -23,3 +23,21 @@ resource "aws_lb_target_group_attachment" "attach" {
   target_id        = data.aws_instance.ec2.id
   port             = 30080
 }
+
+resource "aws_lb_target_group_attachment" "attach" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = data.aws_instance.ec2.id
+  port             = 30081
+}
+
+resource "aws_lb_target_group_attachment" "attach" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = data.aws_instance.ec2.id
+  port             = 30082
+}
+
+resource "aws_lb_target_group_attachment" "attach" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = data.aws_instance.ec2.id
+  port             = 30083
+}
