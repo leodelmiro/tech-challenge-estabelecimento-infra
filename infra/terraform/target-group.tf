2293c6058dc9e98ed7481b86bd14d9a1f602b96d
Leonardo Delmiro <leodelmiroms@gmail.com>
2025-02-14 14:54:06 -0300

    feat: quebrando albs

--- infra/terraform/target-group.tf
+++ infra/terraform/target-group.tf
@@ -1,4 +1,64 @@
-resource "aws_lb_target_group" "tg" {
+resource "aws_lb_target_group" "tg-produto" {
+  name        = "TG-${var.projectName}"
+  port        = 80
+  target_type = "instance"
+  protocol    = "HTTP"
+
+  vpc_id = data.aws_vpc.vpc.id
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 30
+    path                = "/actuator/health"
+    port                = "traffic-port"
+    matcher             = "200"
+    timeout             = 5
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_target_group" "tg-pedido" {
+  name        = "TG-${var.projectName}"
+  port        = 80
+  target_type = "instance"
+  protocol    = "HTTP"
+
+  vpc_id = data.aws_vpc.vpc.id
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 30
+    path                = "/actuator/health"
+    port                = "traffic-port"
+    matcher             = "200"
+    timeout             = 5
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_target_group" "tg-pagamento" {
+  name        = "TG-${var.projectName}"
+  port        = 80
+  target_type = "instance"
+  protocol    = "HTTP"
+
+  vpc_id = data.aws_vpc.vpc.id
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 30
+    path                = "/actuator/health"
+    port                = "traffic-port"
+    matcher             = "200"
+    timeout             = 5
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_target_group" "tg-cliente" {
   name        = "TG-${var.projectName}"
   port        = 80
   target_type = "instance"
@@ -19,25 +79,25 @@ resource "aws_lb_target_group" "tg" {
 }
 
 resource "aws_lb_target_group_attachment" "attach_produto" {
-  target_group_arn = aws_lb_target_group.tg.arn
+  target_group_arn = aws_lb_target_group.tg-produto.arn
   target_id        = data.aws_instance.ec2.id
   port             = 30080
 }
 
 resource "aws_lb_target_group_attachment" "attach_cliente" {
-  target_group_arn = aws_lb_target_group.tg.arn
+  target_group_arn = aws_lb_target_group.tg-cliente.arn
   target_id        = data.aws_instance.ec2.id
   port             = 30081
 }
 
 resource "aws_lb_target_group_attachment" "attach_pagamento" {
-  target_group_arn = aws_lb_target_group.tg.arn
+  target_group_arn = aws_lb_target_group.tg-pagamento.arn
   target_id        = data.aws_instance.ec2.id
   port             = 30082
 }
 
 resource "aws_lb_target_group_attachment" "attach_pedido" {
-  target_group_arn = aws_lb_target_group.tg.arn
+  target_group_arn = aws_lb_target_group.tg-pedido.arn
   target_id        = data.aws_instance.ec2.id
   port             = 30083
 }
