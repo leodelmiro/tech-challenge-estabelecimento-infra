03bf1df687203b3993f706b3dd886f428b488982
Leonardo Delmiro <leodelmiroms@gmail.com>
2025-02-14 15:31:44 -0300

    infra: fmt

--- infra/terraform/target-group.tf
+++ infra/terraform/target-group.tf
@@ -82,26 +82,26 @@ resource "aws_lb_target_group_attachment" "attach_produto" {
   target_group_arn = aws_lb_target_group.tg-produto.arn
   target_id        = data.aws_instance.ec2.id
   port             = 30080
-  depends_on = [aws_lb_target_group.tg-produto]
+  depends_on       = [aws_lb_target_group.tg-produto]
 }
 
 resource "aws_lb_target_group_attachment" "attach_cliente" {
   target_group_arn = aws_lb_target_group.tg-cliente.arn
   target_id        = data.aws_instance.ec2.id
   port             = 30081
-  depends_on = [aws_lb_target_group.tg-cliente]
+  depends_on       = [aws_lb_target_group.tg-cliente]
 }
 
 resource "aws_lb_target_group_attachment" "attach_pagamento" {
   target_group_arn = aws_lb_target_group.tg-pagamento.arn
   target_id        = data.aws_instance.ec2.id
   port             = 30082
-  depends_on = [aws_lb_target_group.tg-pagamento]
+  depends_on       = [aws_lb_target_group.tg-pagamento]
 }
 
 resource "aws_lb_target_group_attachment" "attach_pedido" {
   target_group_arn = aws_lb_target_group.tg-pedido.arn
   target_id        = data.aws_instance.ec2.id
   port             = 30083
-  depends_on = [aws_lb_target_group.tg-pedido]
+  depends_on       = [aws_lb_target_group.tg-pedido]
 }
