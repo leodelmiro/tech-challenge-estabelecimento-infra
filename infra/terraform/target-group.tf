7c62eb7d29f95a60f078c3d12601186174e415a1
Leonardo Delmiro <leodelmiroms@gmail.com>
2025-02-14 09:17:16 -0300

    infra: ajustando nome do recurso

--- infra/terraform/target-group.tf
+++ infra/terraform/target-group.tf
@@ -18,25 +18,25 @@ resource "aws_lb_target_group" "tg" {
   }
 }
 
-resource "aws_lb_target_group_attachment" "attach" {
+resource "aws_lb_target_group_attachment" "attach_produto" {
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = data.aws_instance.ec2.id
   port             = 30080
 }
 
-resource "aws_lb_target_group_attachment" "attach" {
+resource "aws_lb_target_group_attachment" "attach_cliente" {
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = data.aws_instance.ec2.id
   port             = 30081
 }
 
-resource "aws_lb_target_group_attachment" "attach" {
+resource "aws_lb_target_group_attachment" "attach_pagamento" {
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = data.aws_instance.ec2.id
   port             = 30082
 }
 
-resource "aws_lb_target_group_attachment" "attach" {
+resource "aws_lb_target_group_attachment" "attach_pedido" {
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = data.aws_instance.ec2.id
   port             = 30083
